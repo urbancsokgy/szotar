a4cdd38883fe2d9aea2a26f7ce4dd35d1c9de701
urbancsokgy <69484560+urbancsokgy@users.noreply.github.com>
2021-08-20 08:36:03 +0200

    init app

---
+++ backend/helper.md
@@ -0,0 +1,2 @@
+## Start from docker
+docker run --name SzotarDB -p 27017:27017 -v ./database:/bitnami/mongodb bitnami/mongodb:latest
---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose');
+
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
