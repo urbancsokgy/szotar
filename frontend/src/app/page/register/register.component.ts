bb69c413ddeced2fa35caba9a4ed199e6a2df490
urbancsokgy <69484560+urbancsokgy@users.noreply.github.com>
2021-09-18 20:56:35 +0200

    form on the way

--- frontend/src/app/page/register/register.component.ts
+++ frontend/src/app/page/register/register.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { UserWithAddress } from './../../model/user';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { fromEvent, Observable, of, Subject, Subscription } from 'rxjs';
+import { debounceTime, delay, distinctUntilChanged, filter, flatMap, map, mergeMap, switchMap } from 'rxjs/operators';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  inputType: string = 'password';
+  public newUser: any = {};
 
-  constructor() { }
+  public firstPassword: string = ''
+  public passwordRetype: string = ''
+  public passwordCheckFlag: boolean = false
+  public passwordErrorFlag: boolean = false
+
+  constructor() {}
 
   ngOnInit(): void {
+  }
+  submit(form: NgForm){
+    console.log(form.value);
+  }
+
+  setShowHide() {
+    return this.inputType === 'password'
+      ? (this.inputType = 'text')
+      : (this.inputType = 'password');
+  }
+  setPassword(data: string) {
+    this.firstPassword = data;
+  }
+  checkPassword(first: string, retype: string) {
+    this.firstPassword = first
+    this.firstPassword == retype? this.passwordCheckFlag = true : this.passwordCheckFlag = false
+    !this.passwordCheckFlag? this.passwordErrorFlag = true : this.passwordErrorFlag = false
   }
 
 }
